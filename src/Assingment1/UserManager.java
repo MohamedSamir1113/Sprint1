818738809fd9dd591cbd724b217d595849c5b7c1
Hussein-Elshinnawy <clorkthefirst@gmail.com>
2021-11-21 17:42:59 +0200

    add AdminUser

--- src/Assingment1/UserManager.java
+++ src/Assingment1/UserManager.java
@@ -1,4 +1,17 @@
 package Assingment1;
 
-public class UserManager {
+import java.util.ArrayList;
+
+public  class UserManager  {
+
+    Storage s1= new hashmap() {
+        public void register(UserAccount account) {
+            super.register(account);
+        }
+        public boolean login(String usname, String passwd) {
+            return  super.login(usname ,  passwd);
+        }
+
+    };
+
 }
