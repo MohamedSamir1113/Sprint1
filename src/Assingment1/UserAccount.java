dbf9b503f3c1653ffae31a0aa5e126a359fb6851
Samir1113 <94626926+Samir1113@users.noreply.github.com>
2021-11-21 17:32:35 +0200

    add class useraccount

--- src/Assingment1/UserAccount.java
+++ src/Assingment1/UserAccount.java
@@ -1,4 +1,18 @@
 package Assingment1;
 
-public interface UserAccount {
+public class UserAccount {
+    String username ,password ,email ,MobileNumber;
+    public UserAccount()
+    {
+
+    }
+    public UserAccount(String u, String p, String e , String n)
+    {
+        username=u;
+        password=p;
+        email=e;
+        MobileNumber=n;
+        System.out.println("this is a constructor");
+    }
+
 }
