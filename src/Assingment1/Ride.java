fd2d30a8e04fd635fb93cba08a70520a456799dd
Samir1113 <94626926+Samir1113@users.noreply.github.com>
2021-11-21 20:10:06 +0200

    Concrete Subject

--- src/Assingment1/Ride.java
+++ src/Assingment1/Ride.java
@@ -1,16 +1,26 @@
 package Assingment1;
 
-public class Ride extends Driver
+public class Ride implements Subject
 {
     String SourceArea;
     String Destination;
+    Driver d1 = new Driver();
+    public Ride()
+    {
+
+    }
     public Ride(String location , String destination)
     {
         SourceArea=location;
         Destination=destination;
 
     }
-    //notify(locationisfav)
+    public void notifyDriver()
+    {
+        d1.locationisfav(SourceArea);
+        System.out.println("hey Driver i am notifier");
+
+    }
+
 
 }
-
