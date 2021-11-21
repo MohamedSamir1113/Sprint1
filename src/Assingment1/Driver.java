bf0169c73943644158016fa899ba01cf88ec260c
Samir1113 <94626926+Samir1113@users.noreply.github.com>
2021-11-21 17:16:40 +0200

    implement Driver class

--- src/Assingment1/Driver.java
+++ src/Assingment1/Driver.java
@@ -1,4 +1,32 @@
 package Assingment1;
+import java.util.ArrayList;
+public class Driver
+{
+    ArrayList<String> FavAreas = new ArrayList<>();
+    ArrayList<Ride> DriverHistory = new ArrayList();
+    //Ride[] historyofrides;
 
-public class Driver {
+    public ArrayList<String> ListFavAreasRides()
+    {
+        return FavAreas;
+    }
+    public boolean locationisfav(String userlocation)
+    {
+
+        for(String i : FavAreas)
+        {
+            if(i.contentEquals(userlocation)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addfavarea(String newfavarea)
+    {
+        System.out.println(newfavarea);
+        FavAreas.add(newfavarea);
+    }
+
+
 }
