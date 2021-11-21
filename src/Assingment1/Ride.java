89f6ca53ca518de483622c703ab3526a81919c56
Hussein-Elshinnawy <clorkthefirst@gmail.com>
2021-11-21 17:46:53 +0200

    add ride class

--- src/Assingment1/Ride.java
+++ src/Assingment1/Ride.java
@@ -1,4 +1,16 @@
 package Assingment1;
 
-public class Ride {
+public class Ride extends Driver
+{
+    String SourceArea;
+    String Destination;
+    public Ride(String location , String destination)
+    {
+        SourceArea=location;
+        Destination=destination;
+
+    }
+    //notify(locationisfav)
+
 }
+
