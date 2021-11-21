bcc6c9f2cd5a735dc54893fa63d99725a0166f05
Hussein-Elshinnawy <clorkthefirst@gmail.com>
2021-11-21 17:25:02 +0200

    add AdminUser

--- src/Assingment1/AdminUser.java
+++ src/Assingment1/AdminUser.java
@@ -1,4 +1,13 @@
 package Assingment1;
 
 public class AdminUser {
+    public boolean verfiyDriver(String dl, String nid)
+    {
+        if(dl.length()==5 && nid.length()==5 )
+        {
+            System.out.println("valid national id or licence");
+            return true;
+        }
+        return false;
+    }
 }
