2689bdf04a5ae4e6dd3ff355d302850cf77ea35a
Samir1113 <94626926+Samir1113@users.noreply.github.com>
2021-11-21 17:36:10 +0200

    add interface Storage

--- src/Assingment1/Storage.java
+++ src/Assingment1/Storage.java
@@ -1,4 +1,10 @@
 package Assingment1;
 
-public interface Storage {
+public interface Storage
+{
+    //pattern for later
+    public void register(UserAccount account);
+
+    public boolean login(String usname, String passwd);
+
 }
