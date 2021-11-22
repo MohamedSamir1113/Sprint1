2ba25d24b5f44fdcdef3f2cbc97f54cafd74f6a7
Hussein-Elshinnawy <clorkthefirst@gmail.com>
2021-11-22 17:37:35 +0200

    modification on observer

--- src/Assingment1/Ride.java
+++ src/Assingment1/Ride.java
@@ -4,13 +4,23 @@ public class Ride extends Driver
 {
     String SourceArea;
     String Destination;
+    Driver d1 = new Driver();
+    public Ride()
+    {
+
+    }
     public Ride(String location , String destination)
     {
         SourceArea=location;
         Destination=destination;
 
     }
-    //notify(locationisfav)
 
+    public void notifyDriver()
+    {
+        d1.locationisfav(SourceArea);
+        System.out.println("hey Driver i am notifier");
+
+    }
 }
 
